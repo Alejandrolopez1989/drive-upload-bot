82bc088eece5acc6a3ba524a290ed5ad5a936930
Alejandrolopez1989 <140138063+Alejandrolopez1989@users.noreply.github.com>
2024-08-09 02:21:26 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,7 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-import os
+from fastapi import FastAPI
 
-TOKEN = os.environ.get("TOKEN")
+app = FastAPI()
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-def register_handlers(dispatcher):
-    start_handler = CommandHandler('start', start)
-    dispatcher.add_handler(start_handler)
-
-def main():
-    updater = Updater(token=TOKEN, use_context=True)
-    dispatcher = updater.dispatcher
-
-    register_handlers(dispatcher)
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
+@app.get("/")
+def index():
+    return {"message": "Hello World"}
