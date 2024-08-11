aa5c2ed2b29f99c6914c3d08dd5df2284e6d554a
Alejandrolopez1989 <140138063+Alejandrolopez1989@users.noreply.github.com>
2024-08-11 19:09:52 -0400

    Update and rename bot.py to main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+import logging
+from functools import wraps
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text if update.message else update.channel_post.text
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+
+if __name__ == '__main__':
+    token = os.getenv("TELEGRAM_TOKEN")
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("hello", hello))
+    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
+    app.run_polling()
